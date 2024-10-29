8a85dea72c9c7de8a298a2377e630e6abad63f0c
Maximo <maxipreneste@gmail.com>
2024-10-28 21:48:20 -0300

    No puedo hacer un comp

---
+++ components/LogInForm.tsx
@@ -0,0 +1,19 @@
+export default function LogInForm(){
+
+    return (
+        <>
+            
+            <h2>Iniciar seccion</h2>
+            <label htmlFor="">Nombre de usuario</label>
+            <input type="text" />
+            
+            <br />
+            <label htmlFor="">Contraseña</label>
+            <input type="password" />
+            <button>Iniciar seccion</button>
+            <button>Crear Cuenta</button>
+            <a href="">¿Olvidaste tu contraseña?</a>
+        </>
+
+    );
+}
